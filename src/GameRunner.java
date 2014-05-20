578854fbded90b51f5e611858f4d7b50207f48fe
John Wiseheart <johnwiseheart@gmail.com>
2014-05-20 14:22:00 +1000

    working menu

--- src/GameRunner.java
+++ src/GameRunner.java
@@ -1,52 +1,176 @@
 import java.awt.Color;
+import java.awt.Component;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.Graphics2D;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.KeyEventDispatcher;
 import java.awt.KeyboardFocusManager;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
+import javax.imageio.ImageIO;
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.border.Border;
 
 public class GameRunner {
 	GameFrame frame;
-	JPanel buttonPanel;
+	JPanel menuPanel;
+	JPanel gamePanel;
 	
     private GameRunner() {
         gameState = null;
     }
     
     public static void main(String[] args) {
-        new GameRunner().runGame();
+        new GameRunner().runMenu();
     }
     
-    private void runGame() {
-    	buttonPanel = new JPanel();
+    private void runMenu() {
+    	
+    	//add our font
+    	Font joystix = null;
+		try{
+	    	InputStream is = new FileInputStream("joystix.ttf");
+	    	joystix = Font.createFont(Font.TRUETYPE_FONT, is);
+		} catch (Exception e) {
+			
+		}
+		joystix = joystix.deriveFont(36f);
+    	
+		//create our green color
+		Color ourGreen = new Color(0xA1FF9C);
+		
+		
+		menuPanel = new JPanel();
+		
+		BufferedImage image = null;
+		try {
+			image = ImageIO.read(new File("wobby.png"));
+			JLabel wobman = new JLabel(new ImageIcon(image));
+			wobman.setAlignmentX(Component.CENTER_ALIGNMENT);
+			
+			
+			
+			Border paddingBorder = BorderFactory.createEmptyBorder(70,0,100,0);
+			wobman.setBorder(paddingBorder);
+			
+			menuPanel.add(wobman);
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+    	
     	JButton button = new JButton("Play");
-    	buttonPanel.add(button);    	
+    	button.setFont(joystix);
+    	button.setForeground(ourGreen);
+    	button.setBackground(Color.black);
+    	button.setOpaque(true);
+    	button.setBorderPainted(false);
+    	
+		button.addActionListener(new
+            ActionListener()
+            {
+               public void actionPerformed(ActionEvent event)
+               {
+            	  frame.remove(menuPanel);
+            	  runGame();
+               }
+            });
+    	
+    	button.setAlignmentX(Component.CENTER_ALIGNMENT);
+    	button.setFont(joystix);
+    	button.setForeground(ourGreen);
+    	button.setBackground(Color.black);
+    	button.setOpaque(true);
+    	button.setBorderPainted(false);
+    	menuPanel.add(button);
+    	button = new JButton("Options");
+    	button.setAlignmentX(Component.CENTER_ALIGNMENT);
+    	button.setFont(joystix);
+    	button.setForeground(ourGreen);
+    	button.setBackground(Color.black);
+    	button.setOpaque(true);
+    	button.setBorderPainted(false);
+    	menuPanel.add(button);
+    	button = new JButton("Controls");
+    	button.setAlignmentX(Component.CENTER_ALIGNMENT);
+    	button.setFont(joystix);
+    	button.setForeground(ourGreen);
+    	button.setBackground(Color.black);
+    	button.setOpaque(true);
+    	button.setBorderPainted(false);
+    	menuPanel.add(button);
+    	button = new JButton("Exit");
+    	button.setAlignmentX(Component.CENTER_ALIGNMENT);
+    	button.setFont(joystix);
+    	button.setForeground(ourGreen);
+    	button.setBackground(Color.black);
+    	button.setOpaque(true);
+    	button.setBorderPainted(false);
+    	menuPanel.add(button);
+    	
+    	menuPanel.setLayout(new BoxLayout(menuPanel, BoxLayout.PAGE_AXIS));
+    	
     	
     	frame = new GameFrame();
         frame.setBackground(Color.black);
-        //frame.setLayout(new FlowLayout());
-        
-        MazeDisplay mazeDisplay = new MazeDisplay();
+       
+        frame.add(menuPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800, 600);
+        frame.setVisible(true);
+    	
+    }
+    
+    private void runGame() {
+    	
+    	gamePanel = new JPanel();
+    	
+    	
+    	
+    	MazeDisplay mazeDisplay = new MazeDisplay();
         gameState = new GameState(31, 23, mazeDisplay);
-        
-        //frame.add(buttonPanel);
+
         frame.add(mazeDisplay);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(800, 600);
         frame.setVisible(true);
- 
+        
+         
+         //we tick in a new thread so we can do other stuff
+        Runnable r1 = new Runnable() {
+        	public void run() {
+        		while (true) {
+				    gameState.tickPlayer();
+				    gameState.tickEnemies();
+				    try {
+				        Thread.sleep(200);
+				    } catch (InterruptedException e) {
+				        // Doesn't happen.
+				    }
+				}
+        	}
+        };
+    	Thread thr1 = new Thread(r1);
+    	thr1.start();
         // Game update loop.
-        while (true) {
-            gameState.tickPlayer();
-            gameState.tickEnemies();
-            try {
-                Thread.sleep(200);
-            } catch (InterruptedException e) {
-                // Doesn't happen.
-            }
-        }
+        
     }
     
     public class GameFrame extends JFrame {    
@@ -71,6 +195,8 @@ public class GameRunner {
                 			System.out.println("Down");
                 			gameState.setPlayerV(0, 1);
                 			break;
+                		case KeyEvent.VK_P:
+                			
                 	} 
                 }
                 return false;
